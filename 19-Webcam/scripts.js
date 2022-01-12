3e97f09e19cd587aa53ea0e315b09d8a43673541
redfox <76628594+purplehippo911@users.noreply.github.com>
2022-01-12 10:51:29 +0100

    -a

--- 19-Webcam/scripts.js
+++ 19-Webcam/scripts.js
@@ -27,7 +27,12 @@ function paintToCanvas() {
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // mess with them
-        pixels = redEffect(pixels);
+        // pixels = redEffect(pixels);
+
+        //   pixels = rgbSplit(pixels);
+        //   ctx.globalAlpha = 0.8;
+
+        pixels = greenScreen(pixels);
         // put them back
         ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -55,7 +60,41 @@ function redEffect(pixels) {
     }
     return pixels;
 }
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i+=4) {
+        pixels.data[i - 150] = pixels.data[i + 0] ; // RED
+        pixels.data[i + 700] = pixels.data[i + 1] ; // GREEN
+        pixels.data[i - 550] = pixels.data[i + 2] ; // Blue
+    };
+    return pixels;
+}
 
+function greenScreen(pixels){
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+      });
+      console.log(levels);
+    
+      for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    
+        if (red >= levels.rmin
+          && green >= levels.gmin
+          && blue >= levels.bmin
+          && red <= levels.rmax
+          && green <= levels.gmax
+          && blue <= levels.bmax) {
+          // take it out!
+          pixels.data[i + 3] = 0;
+        }
+    }
+    return pixels;
+}
 getVideo();
 
 
